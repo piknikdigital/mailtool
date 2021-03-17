886265166c2fcf3037fec65ab0cda1cff371b717
Armando Romero <armando.romero@piknik.com.mx>
2021-03-17 02:26:06 -0600

    .

---
+++ app/cli/_JSON/_PREVIOS/_2021/21-30-data/21.30-json-arguments-FSH.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once("21.30-common.php");
+
+$target     = "FSH";          // "HTML", "DCE" o "FSH"
+$source     = "HTML";         // "HTML", "DCE" o "FSH"
+
+// Definition of replacements to be made
+$filename1  = "_JSON/21-30-data/21.30-replacements.json";      
+
+
+/**
+ * VARS BELOW ARE ALMOST ALWAYS THE SAME
+ */
+
+$filename   = $filename_base . $target;
+$subject    = $subject_base;
+$preheader  = $preheader_base;
+
+switch ($target)
+{
+    case "DCE":  $path = $path_DCE; $nameincrm = $name_in_DCE; $customerkey=$customerkey_DCE; break;
+    case "FSH":  $path = $path_FSH; $nameincrm = $name_in_FSH; $customerkey=$customerkey_FSH; break;
+    default:     $path = "";        $nameincrm = "";           $customerkey="" ; break;
+}
+
