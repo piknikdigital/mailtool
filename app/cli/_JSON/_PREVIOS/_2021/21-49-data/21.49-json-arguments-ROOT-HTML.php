589fa265244f107d906cc18da27072c2a4b776b7
Armando Romero <armando.romero@piknik.com.mx>
2021-04-15 18:36:35 -0500

    .

---
+++ app/cli/_JSON/_PREVIOS/_2021/21-49-data/21.49-json-arguments-ROOT-HTML.php
@@ -0,0 +1,199 @@
+<?php
+
+require_once("21.49-common.php");
+
+$target    = "HTML";       // "HTML", "DCE" o "FSH"
+
+$filename  = $filename_base . $target;
+$subject   = $subject_base;
+$preheader = $preheader_base;
+
+switch ($target)
+{
+    case "DCE":  $path = $path_DCE; $nameincrm = $name_in_DCE; $customerkey=$customerkey_DCE; break;
+    case "FSH":  $path = $path_FSH; $nameincrm = $name_in_FSH; $customerkey=$customerkey_FSH; break;
+    default:     $path = "";        $nameincrm = "";           $customerkey="" ; break;
+}
+
+$requiredBlocks = array(
+    "img_Logo_Header_HTML",
+    "img_Cover_HTML",
+    "spacer",
+    "greeting_negro",
+    "p300",
+    "pframed",
+    "m_IMG_TXT_m",
+    "graylineframed",
+    "m_IMG_TXT_m",
+    "graylineframed",
+    "m_IMG_TXT_m",
+    "graylineframed",
+    "pframed",
+    "m_IMG_TXT_m",
+    "m_IMG_TXT_m",
+    "m_IMG_TXT_m",
+    "m_IMG_TXT_m",
+    "pframed",
+    "p300",
+    "pframed",
+    "p300",
+    "cta1",
+    "p300",
+    "p700",
+    "disclaimer_iqos"    
+);
+
+
+/* 
+    "img",
+    "spacer",
+    "pframed",
+    "cta1",
+    "greeting_negro",
+    "greeting_dorado",
+    "p300",
+    "p700",
+
+    "img_Logo_Header_HTML",
+    "img_Cover_HTML",
+    "rawHTML",
+    "m_TXT_TXT_m",
+    "m_TXT_IMG_m",
+    "m_IMG_TXT_m",
+    "m_IMG_IMG_m",
+    "btn_sa_comprar_HTML",
+    "one_rs",
+    "two_rs",
+    "three_rs",
+    "sinfuego_sinhumo_sinceniza_negro_HTML",
+    "sinfuego_sinhumo_sinceniza_dorado_HTML",
+    "legal_extra",
+    "disclaimer_iqos",
+    "cols_33_33_33",
+    "form",
+    "mgm_coupon_1",
+    "mgm_coupon_2cols",
+    "rgm_coupon_2cols",
+    "cta_share_WA_coupon",
+    "graylineframed",
+    "shopping_list",
+    "3cols_chat_fb_phone",
+
+    CODIGO PARA CUPON X X X X X X X X :   %%=v(@voucher_code)=%% 
+    "rawHTML", // <div data-type=\"slot\" data-key=\"bspwz932ek9\" data-label=\"Drop blocks or content here\">
+    "rawHTML", // %%=ContentBlockbyKey(&quot;Onboarding_Conversion_Check&quot;)=%% 
+    $a= "p300|^|f7f7f7|@|Queremos que vivas la mejor experiencia <span style=\"color: #e4ab68; font-weight: 700;\">IQOS</span>, por esa razón te presentamos las medidas de seguridad que tendremos en todas nuestras <span style=\"color: #e4ab68; font-weight: 700;\">Tiendas IQOS</span>.";
+*/
+
+
+/*
+BAD CHR : "", "	"
+--------------------
+--------------------
+Estamos muy emocionados de invitarte a formar parte de nuestro próximo Instagram Live.<br>Tú has sido parte importante del cambio y queremos que nos cuentes acerca de tu experiencia durante la transmisión.
+--------------------
+Durante la transmisión, te haremos 3 preguntas para que nos cuentes cómo ha sido tu experiencia con IQOS. Las preguntas serán las siguientes:
+3087bd
+--------------------
+1
+¿Qué pensaste la primera vez que viste un IQOS?
+--------------------
+2
+¿Qué opinan tus amigos fumadores adultos de que usas IQOS?
+--------------------
+3
+Según tu experiencia, ¿cuál es la razón más importante por la que más fumadores adultos deberían de cambiar a IQOS?
+--------------------
+Te sugerimos tomar en cuenta las siguientes consideraciones durante tu participación:
+185a99
+--------------------
+-1- color: 185a99 / bgcolor: e1e0e2
+No hacer mención de la marca de las unidades de tabaco para calentar.
+--------------------
+-2- color: FFFFFF / bgcolor: b8b7bb
+En tus respuestas, evita relacionar tu experiencia con temas de salud. Si lo prefieres, puedes ahondar en beneficios como menos olor, sin ceniza, sin humo.
+--------------------
+-3- color: 185a99 / bgcolor: e1e0e2
+Al ser IQOS un producto para mayores de edad, durante tu participación por favor contempla que no pueden aparecer menores de edad.
+--------------------
+-4- color: FFFFFF / bgcolor: b8b7bb
+Ten en cuenta que debes tener una cuenta de Instagram activa para unirte a la transmisión. Serás invitado por parte de @iqos_mx para que seas conectado.
+--------------------
+Te contactaremos en los siguientes días para comunicarte los siguientes pasos.<br>Te recordamos el horario:
+--------------------
+15 de abril 2021<br>7:00 pm (Ciudad de México)
+3087bd
+--------------------
+Si aún no nos sigues, encuentra la cuenta de IQOS aquí.
+--------------------
+
+https://www.instagram.com/iqos_mx/
+--------------------
+¡Te agradecemos en grande tu participación!
+--------------------
+CALIENTA TABACO REAL.<br>MENOS OLOR. SIN HUMO. SIN CENIZA.
+0a2354
+--------------------
+--------------------
+--------------------
+--------------------
+<a href=\"LINK\" style=\"text-decoration:underline;\" target=\"_blank\">TEXTO</a>
+
+<br>
+<br><br>
+<b></b>
+
+<sup>(1)</sup>
+<sup>(2)</sup>
+<sup>(3)</sup>
+<sup>(4)</sup>
+
+<span style=\"font-size:12px;\"><sup>(1)</sup></span>
+<span style=\"font-size:12px;\"><sup>(2)</sup></span>
+<span style=\"font-size:12px;\"><sup>(3)</sup></span>
+<span style=\"font-size:12px;\"><sup>(4)</sup></span>
+
+
+<b>IQOS</b>
+<b>Kit IQOS</b>
+<b>Tiendas IQOS</b>
+<b>IQOS 2.4 PLUS</b>
+<b></b>
+<b></b>
+<b></b>
+
+
+<span style=\"color: #e4ab68; font-weight: 700;\">IQOS EL RETO</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">IQOS EL RETO<sup>(1)</sup></span>
+<span style=\"color: #e4ab68; font-weight: 700;\">KIT IQOS</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">IQOS</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">iqos.com</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">350&deg;C</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">¡$300 MXN de descuento!</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">$300 MXN de descuento</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">$300 MXN</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">HeatControl&trade; Technology</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">¿Aún no te has convencido?</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">¿Todavía lo estás pensando?</span>
+<span style=\"color: #e4ab68; font-weight: 700;\">gratis</span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-weight: 700;\"></span>
+
+<span style=\"color: #e4ab68; font-size:24px; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-size:24px; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-size:24px; font-weight: 700;\"></span>
+<span style=\"color: #e4ab68; font-size:24px; font-weight: 700;\"></span>
+
+<span style=\"font-size:24px; font-weight: 700;\"></span>
+<span style=\"font-size:24px; font-weight: 700;\"></span>
+<span style=\"font-size:24px; font-weight: 700;\">35% de descuento</span>
+<span style=\"font-size:24px; font-weight: 700;\">IQOS 2.4 PLUS</span>
+
+
+*/
---
+++ app/cli/_JSON/_PREVIOS/_2021/21-52-data/21.52-common.php
@@ -0,0 +1,59 @@
+<?php
+
+$p_id =  '52';
+$p_subject =  'Gracias por ser parte de Futuro Sin Humo';
+$p_preheader =  'Juntos haremos de esto una realidad';
+$p_yy =  '21';
+$p_mm =  '04';
+$p_dd =  '06';
+$p_name4folder =  'fsh-v2';
+$p_name4json =  'fsh-transaccional-v2';
+$p_name4folderDCE =  'NA';
+$p_mailname4DCE =  'NA';
+$p_customerkey_DCE =  'NA';
+$p_name4folderFSH =  'FSH';
+$p_mailname4FSH =  'FSH Transaccional v2';
+$p_customerkey_FSH =  '31442acd-09af-416d-a59a-df362592c698';
+$id_FSH_folder =  '112';
+
+//-----------------------------------------
+
+//Data of target to be created
+$mes             = "pmi"  .$p_yy.$p_mm;
+$folder          = "pmi-" .$p_yy.$p_mm.$p_dd. "-" .$p_name4folder;
+$filename_base   = "pmi-" .$p_yy.$p_mm.$p_dd. "-ID" .$p_yy. "." .$p_id. "-" .$p_name4json. "-";
+
+//Texts for CRM
+$subject_base     = trim($p_subject);
+$preheader_base   = trim($p_preheader);
+
+$path_DCE         = "Content Builder > Mexico > Commercial > MX_20" .$p_yy.$p_mm.$p_dd.  "_" . $p_name4folderDCE ." > 01_HTML";
+$name_in_DCE      = $p_mailname4DCE;
+$customerkey_DCE  = $p_customerkey_DCE;
+
+//$path_FSH         = "Content Builder > YY_NAME4CRM DD MES 2020 > 02_HTML";
+$path_FSH         = "Content Builder > ". $id_FSH_folder .  "_" .$p_name4folderFSH. " " . $p_dd . " " .$p_mm. " 20" .$p_yy. " > 02_HTML";
+$name_in_FSH      = $p_mailname4FSH;
+$customerkey_FSH  = $p_customerkey_FSH;
+
+$TwigTemplate     = "pmi-blocks-mod-B01-600-v02.twig";
+$TwigFooterBlock  = "block-footer-no-youtube.twig";
+
+$footergrey       = "1";          // 1:grey  0:white
+$bgColor          = "#FFFFFF";    // Don't use "transparent"
+
+// E N D  -  C O N F I G
+
+
+// TEST VARS
+// echo $mes             . PHP_EOL;
+// echo $folder          . PHP_EOL;
+// echo $filename_base   . PHP_EOL;
+// echo "SUBJECT"                    . PHP_EOL . $subject_base    . PHP_EOL ;
+// echo "PREHEADER"                  . PHP_EOL . $preheader_base  . PHP_EOL ;
+// echo "RUTA DCE"                   . PHP_EOL . $path_DCE        . PHP_EOL ;
+// echo "RUTA CONTENT BUILDER LOCAL" . PHP_EOL . $path_FSH        . PHP_EOL. PHP_EOL ;
+
+
+
+
