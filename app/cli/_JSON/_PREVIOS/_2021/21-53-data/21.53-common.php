33befcebd5efcb295f66957f2e5582033d63bd79
Armando Romero <armando.romero@piknik.com.mx>
2021-04-21 14:26:16 -0500

    .

---
+++ app/cli/_JSON/_PREVIOS/_2021/21-53-data/21.53-common.php
@@ -0,0 +1,59 @@
+<?php
+
+$p_id =  '52';
+$p_subject =  'Tenemos información importante sobre nuestras Tiendas';
+$p_preheader =  'Da click y conócela';
+$p_yy =  '21';
+$p_mm =  '04';
+$p_dd =  '21';
+$p_name4folder =  'cierre-tiendas';
+$p_name4json =  'cierre-tiendas';
+$p_name4folderDCE =  'Stores';
+$p_mailname4DCE =  'Cierre de Tiendas';
+$p_customerkey_DCE =  'NA';
+$p_name4folderFSH =  'Stores';
+$p_mailname4FSH =  'Cierre de tiendas';
+$p_customerkey_FSH =  '12569849-09e3-4a26-9b60-b1d70d7a2dca';
+$id_FSH_folder =  '114';
+
+//-----------------------------------------
+
+//Data of target to be created
+$mes             = "pmi"  .$p_yy.$p_mm;
+$folder          = "pmi-" .$p_yy.$p_mm.$p_dd. "-" .$p_name4folder;
+$filename_base   = "pmi-" .$p_yy.$p_mm.$p_dd. "-ID" .$p_yy. "." .$p_id. "-" .$p_name4json. "-";
+
+//Texts for CRM
+$subject_base     = trim($p_subject);
+$preheader_base   = trim($p_preheader);
+
+$path_DCE         = "Content Builder > Mexico > Commercial > MX_20" .$p_yy.$p_mm.$p_dd.  "_" . $p_name4folderDCE ." > 01_HTML";
+$name_in_DCE      = $p_mailname4DCE;
+$customerkey_DCE  = $p_customerkey_DCE;
+
+//$path_FSH         = "Content Builder > YY_NAME4CRM DD MES 2020 > 02_HTML";
+$path_FSH         = "Content Builder > ". $id_FSH_folder .  "_" .$p_name4folderFSH. " " . $p_dd . " " .$p_mm. " 20" .$p_yy. " > 02_HTML";
+$name_in_FSH      = $p_mailname4FSH;
+$customerkey_FSH  = $p_customerkey_FSH;
+
+$TwigTemplate     = "pmi-blocks-mod-B01-600-v02.twig";
+$TwigFooterBlock  = "block-footer-no-youtube.twig";
+
+$footergrey       = "1";          // 1:grey  0:white
+$bgColor          = "#FFFFFF";    // Don't use "transparent"
+
+// E N D  -  C O N F I G
+
+
+// TEST VARS
+// echo $mes             . PHP_EOL;
+// echo $folder          . PHP_EOL;
+// echo $filename_base   . PHP_EOL;
+// echo "SUBJECT"                    . PHP_EOL . $subject_base    . PHP_EOL ;
+// echo "PREHEADER"                  . PHP_EOL . $preheader_base  . PHP_EOL ;
+// echo "RUTA DCE"                   . PHP_EOL . $path_DCE        . PHP_EOL ;
+// echo "RUTA CONTENT BUILDER LOCAL" . PHP_EOL . $path_FSH        . PHP_EOL. PHP_EOL ;
+
+
+
+
