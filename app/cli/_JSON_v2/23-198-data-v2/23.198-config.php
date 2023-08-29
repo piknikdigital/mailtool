5b9abc675ec334f478766da28ca9ae422ab3f037
Armando Romero <57216367+sentiko@users.noreply.github.com>
2023-08-28 18:50:05 -0600

    .

---
+++ app/cli/_JSON_v2/23-198-data-v2/23.198-config.php
@@ -0,0 +1,284 @@
+<?php
+
+$p_id =  '198';
+$p_subject =  '¿Cuál es el principal problema del cigarro? 🤔  ';
+$p_preheader =  'Descubre porque el calentamiento de tabaco es la solución al principal problema de consumo';
+$p_yy =  '23';
+$p_mm =  '08';
+$p_dd =  '22';
+$p_name4folder =  'leads-originals-learn-more';
+$p_name4json =  'learn-more-science-3-menos-olor';
+$p_name4folderDCE =  'NA';
+$p_mailname4DCE =  'NA';
+$p_customerkey_DCE =  'NA';
+$p_name4folderFSH =  'LEADS Originals Learn More';
+$p_mailname4FSH =  'Learn More - Science 3 MENOSOLOR';
+$p_customerkey_FSH =  'NA';
+$id_FSH_folder =  '187';
+
+//-----------------------------------------
+
+$companyBrand     = "pmi";
+$TwigTemplate     = "pmi-Brand-World-B01-600-v01.twig";
+$bgColor          = "#FFFFFF";    // Don't use "transparent"
+
+//-----------------------------------------
+
+$requiredBlocks = array(
+    "header_brand_world",
+    "columns",
+    "greeting_v2",
+
+    "columns",
+    "p700_BW",
+    "columns",
+
+    "legal_extra",
+    "disclaimer_iqos",
+
+    "cta1_v2",
+
+    "p700_BW",
+    "p300_BW",
+    "img",
+    "p300_BW",
+
+    "cta1_v2",
+
+    "columns",
+    "img",
+
+    "ElijoIQOS",
+    "legal_extra",
+    "disclaimer_iqos",
+    "footer_bw_v1_lowercase"
+);
+
+
+/* 
+    "img",
+    "spacer",
+    "pframed",
+    "cta1",
+    "p_v2",
+    "p300_BW",
+    "p700_BW",
+    "greeting_v2",
+    "columns",
+    "cta1_v2",
+    "spacer_v2",
+
+    "footer_bw_v1",
+
+        PARA FUTURO SIN HUMO USAR:
+            "disclaimer_fsh",
+            "footer_fsh"
+
+        PARA IQOS USAR
+            "ElijoIQOS",
+            "legal_extra",
+            "disclaimer_iqos",
+            "footer_bw_v1_lowercase"
+
+
+
+    "greeting_negro",
+    "greeting_dorado",
+    "footer_brand_world",
+    "header_brand_world",
+
+    "FSH_Yellow_Share",
+    "FSH_Yellow_Share_2",
+    "FSH_Framed_Card",
+
+
+    "p300",
+    "p700",
+    "img_Logo_Header_HTML",
+    "img_Cover_HTML",
+    "rawHTML",
+    "m_TXT_TXT_m",
+    "m_TXT_IMG_m",
+    "m_IMG_TXT_m",
+    "m_IMG_IMG_m",
+    "btn_sa_comprar_HTML",
+    "one_rs",
+    "two_rs",
+    "three_rs",
+    "sinfuego_sinhumo_sinceniza_negro_HTML",
+    "sinfuego_sinhumo_sinceniza_dorado_HTML",
+    "legal_extra",
+    "disclaimer_iqos",
+    "cols_33_33_33",
+    "form",
+    "mgm_coupon_1",
+    "mgm_coupon_2cols",
+    "rgm_coupon_2cols",
+    "cta_share_WA_coupon",
+    "graylineframed",
+    "shopping_list",
+    "3cols_chat_fb_phone",
+
+    PREVIO, YA NO USAR: "footer_brand_world"
+    CODIGO PARA CUPON 198198XX :   %%=v(@voucher_code)=%% 
+
+    "rawHTML", // <div data-type=\"slot\" data-key=\"bspwz932ek9\" data-label=\"Drop blocks or content here\">
+    "rawHTML", // %%=ContentBlockbyKey(&quot;Onboarding_Conversion_Check&quot;)=%% 
+
+
+*/
+
+
+/*
+BAD CHR : "", "	"
+" ",
+--------------------
+--------------------
+url para mandar SMS:
+    https://cutt.ly/59uY4Ga
+Shortlink points to:
+    sms://+525592253374
+--------------------
+PRECIO TACHADO:
+<span style=\"text-decoration:line-through;color:#00d1d2;\">
+   <span style=\"color:#34303d;\">
+   &nbsp;$1,399 MXN&nbsp;
+   </span>
+</span>
+********************************************************************
+********************************************************************
+
+++---------------------------------++
+||   COLOR TURQUESA Brand World    ||
+||   00d1d2                        ||
+||   COLOR SLATE  Brand World      ||
+||   34303d                        ||
+||   Naranja                       ||
+||   e3813b                        ||
+||   GRIS CLARO                    ||
+||   faf7f4                        ||
+++---------------------------------++
+--------------------
+===================================================================
+<span style=\"color:#00d1d2;font-weight:700;\"></span>
+===================================================================
+<span style=\"color:#00d1d2;font-weight:700;font-size:24px;\"></span>
+===================================================================
+<span style=\"font-size:24px;font-weight:700;\"></span>
+===================================================================
+<a href=\"LINK\" style=\"text-decoration:underline;\" target=\"_blank\">TEXTO</a>
+===================================================================
+<span style=\"font-size:12px;\"><sup>(1)</sup></span>
+<span style=\"font-size:12px;\"><sup>(2)</sup></span>
+<span style=\"font-size:12px;\"><sup>(3)</sup></span>
+<span style=\"font-size:12px;\"><sup>(4)</sup></span>
+<span style=\"font-size:12px;\"><sup>MXN</sup></span>
+===================================================================
+--------------------
+--------------------
+¿Sabías que el <br>principal problema <br>de fumar es <br><span style=\"color:#34303d;\">la combustión?</span>
+--------------------
+--------------------
+--------------------
+--------------------
+Al quemarse el tabaco, emite <br>sustancias químicas nocivas, además <br>deja olor y sabor a humo de cigarro.
+--------------------
+--------------------
+IQOS al calentar tabaco en <br>lugar de quemarlo 
+--------------------
+--------------------
+Produce 95% menos sustancias <br>tóxicas en comparación con <br>los cigarros.
+--------------------
+--------------------
+--------------------
+*95% menos representa las reducciones promedio en los niveles de una variedad de sustancias químicas nocivas (excluyendo la nicotina) en comparación con el humo de un cigarro de referencia (3R4F)
+--------------------
+<b>Información importante:</b> No necesariamente equivale a un 95% de reducción en riesgo.<br> IQOS no es libre de riesgo.
+--------------------
+--------------------
+--------------------
+¿Cómo es la experiencia de uso con IQOS?
+--------------------
+IQOS utiliza unidades de tabaco para calentar diseñadas con tabaco de las más alta calidad, las calienta a temperaturas controladas permitiendo su uso en promedio por:
+--------------------
+--------------------
+IQOS es un <span style=\"color:#00d1d2;font-weight:700;\">dispositivo recargable</span> que te permite <span style=\"color:#00d1d2;font-weight:700;\">usar hasta 20 unidades de tabaco para calentar con cada recarga.</span>
+--------------------
+--------------------
+--------------------
+Recuerda utilizar tu código de
+¡10% de descuento(1)!
+Válido en la compra de 
+tu primer kit IQOS.
+--------------------
+--------------------
+--------------------
+Solo con IQOS
+--------------------
+--------------------
+--------------------
+--------------------
+(1) Promoción vigente del 1 de enero al 31 de diciembre del 2023. Código de 10% de descuento redimible solo una vez por usuario registrado (con cuenta creada en www.iqos.com). Válido únicamente en la compra de tu primer Kit IQOS en la República Mexicana. No es válido en la compra de unidades de tabaco para calentar ni en accesorios IQOS. Solo para fumadores adultos o usuarios adultos de otros productos con nicotina. Vigencia del código del 1 de enero al 31 de diciembre del 2023. Aplicable en compras realizadas en www.iqos.com, Tiendas IQOS y asesores IQOS. Sujeto a disponibilidad de inventario. El código deberá ser canjeado en su totalidad en una sola exhibición. No es acumulable con otros códigos de descuento y no será canjeable por dinero en efectivo ni cualquier otra forma de pago.
+--------------------
+--------------------
+--------------------
+--------------------
+--------------------
+--------------------
+--------------------
+--------------------
+===================================================================
+SHARE MSG FSH:
+
+fb
+Hablemos para que 15 millones de fumadores puedan elegir. Hablemos de alternativas para los que fuman. Únete al movimiento. https://futurosinhumo.com.mx 
+http://www.facebook.com/share.php?u=https://futurosinhumo.com.mx&quote=Hablemos%20para%20que%2015%20millones%20de%20fumadores%20puedan%20elegir.%20Hablemos%20de%20alternativas%20para%20los%20que%20fuman.%20%C3%9Anete%20al%20movimiento.
+
+tw
+Hablemos para que 15 millones de fumadores puedan elegir. Hablemos de alternativas para los que fuman. Únete al movimiento. https://futurosinhumo.com.mx 
+https://twitter.com/intent/tweet?text=Hablemos%20para%20que%2015%20millones%20de%20fumadores%20puedan%20elegir.%20Hablemos%20de%20alternativas%20para%20los%20que%20fuman.%20%C3%9Anete%20al%20movimiento.%20https%3A%2F%2Ffuturosinhumo.com.mx
+ya no es necesario: https://ctt.ac/z13Mr
+
+wa
+Hablemos para que 15 millones de fumadores puedan elegir. Hablemos de alternativas para los que fuman. Únete al movimiento. https://futurosinhumo.com.mx 
+https://wa.me/?text=Hablemos%20para%20que%2015%20millones%20de%20fumadores%20puedan%20elegir.%20Hablemos%20de%20alternativas%20para%20los%20que%20fuman.%20%C3%9Anete%20al%20movimiento.%20https%3A%2F%2Ffuturosinhumo.com.mx
+
+===================================================================
+
+"donde_codificar_la_siguiente url" : "https://www.urlencoder.org/",
+"url_ctaDECODED"     : "whatsapp://send?text=",
+"text2encode"        : "Te regalo 20% de descuento en la compra de tu primer Kit IQOS. No olvides utilizar mi código %%=v(@voucher_code)=%% al comprar.",
+"imgulr1"            : "whatsapp://send?text=",
+"imgulr2"            : "https://wa.me/?text=",
+"imgulr3"            : "https://web.whatsapp.com/send?text=",
+"imgulr"             : "https://wa.me/?text=Te%20regalo%2020%25%20de%20descuento%20en%20la%20compra%20de%20tu%20primer%20Kit%20IQOS.%0ANo%20olvides%20utilizar%20mi%20c%C3%B3digo%20%25%25%3Dv%28%40voucher_code%29%3D%25%25%20al%20comprar.",
+
+===================================================================
+
+LEGAL FSH  - Brand World
+&copy; 2021 %%Member_Busname%% Todos los derechos reservados.<br>%%Member_Addr%%,<br>%%Member_City%%, %%Member_PostalCode%%, %%Member_State%%, %%Member_Country%%<br><a href=\"%%unsub_center_url%%\"><span style=\"text-decoration:underline;color:blue;\">Darme de baja</span></a> | <a href=\"%%profile_center_url%%\"><span style=\"text-decoration:underline;color:blue;\">Actualizar perfil</span></a>
+
+===================================================================
+--------------------
+--------------------
+--------------------
+--------------------
+--------------------
+
+<br>
+<br><br>
+<b></b>
+
+
+
+<b>IQOS</b>
+<b>Kit IQOS</b>
+<b>Tiendas IQOS</b>
+<b>IQOS 2.4 PLUS</b>
+<b>iqos.com</b>
+
+
+
+
+
+*/
